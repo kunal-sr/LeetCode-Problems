44aabd48040833981094681f74c3f1a804789704
Kunal Srivastava <srivastavakunal8810@gmail.com>
2026-04-19 09:38:34 +0530

    Create maximum-distance-between-a-pair-of-values.cpp

---
+++ Two Pointer/maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0, j = 0;
+        int n = nums1.size(), m = nums2.size();
+        int maxDist = 0;
+
+        while (i < n && j < m) {
+            if (nums1[i] <= nums2[j]) {
+                maxDist = max(maxDist, j - i);
+                j++;  // try to expand distance
+            } else {
+                i++;  // move i to find valid pair
+            }
+        }
+
+        return maxDist;
+    }
+};
